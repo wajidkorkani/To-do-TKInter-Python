b6d0cd631edd3c616f23f77b7e77dca5c57c8217
wajidkorkani <errormrcoder@gmail.com>
2026-03-20 09:01:50 +0500

    Add button and input in a row.

--- Src/app.py
+++ Src/app.py
@@ -1,24 +1,14 @@
 import customtkinter as ctk
 
-# To-do list
-To_Do = []
-
-# Create window
 app = ctk.CTk()
-app.title("To-Do")
-app.geometry("600x600")
+app.geometry("300x200")
 
-# Frame for tasks
-task_frame = ctk.CTkFrame(app)
-task_frame.pack(pady=20, expand=True)
-task_frame._bg_color = "white"
+# Entry widget
+entry = ctk.CTkEntry(app, placeholder_text="Enter task", width=600, height=50)
+entry.grid(row=0, column=4, padx=20, pady=20)
 
-# Entry box
-entry = ctk.CTkEntry(app, placeholder_text="Enter new task")
-entry.pack(pady=20)
-
-# Add button
-add_btn = ctk.CTkButton(app, text="Add Task", command=print("Task added"))
-add_btn.pack(pady=5)
+# Button widget
+button = ctk.CTkButton(app, text="Add Task", width=100, height=50, bg_color="darkgreen", fg_color="darkgreen", hover_color="#45a049", corner_radius=10)
+button.grid(row=0, column=5, padx=20, pady=20)
 
 app.mainloop()
