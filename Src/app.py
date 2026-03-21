a2bc095d9521ab5ec60bfe347fb47077238f71d3
wajidkorkani <errormrcoder@gmail.com>
2026-03-21 13:55:51 +0500

    Center input and button vertically and horizontally.

--- Src/app.py
+++ Src/app.py
@@ -3,12 +3,14 @@ import customtkinter as ctk
 app = ctk.CTk()
 app.geometry("300x200")
 
+frame = ctk.CTkFrame(app)
+frame.pack(expand=True)
 # Entry widget
-entry = ctk.CTkEntry(app, placeholder_text="Enter task", width=600, height=50)
-entry.grid(row=0, column=4, padx=20, pady=20)
+entry = ctk.CTkEntry(frame, placeholder_text="Enter task", width=600, height=50)
+entry.grid(row=0, column=0, padx=10)
 
 # Button widget
-button = ctk.CTkButton(app, text="Add Task", width=100, height=50, bg_color="darkgreen", fg_color="darkgreen", hover_color="#45a049", corner_radius=10)
-button.grid(row=0, column=5, padx=20, pady=20)
+button = ctk.CTkButton(frame, text="Add Task", width=100, height=50, bg_color="darkgreen", fg_color="darkgreen", hover_color="#45a049", corner_radius=10)
+button.grid(row=0, column=1, padx=10)
 
 app.mainloop()
