8375a9c0186628ac6fc8a069170747c21e998629
wajidkorkani <errormrcoder@gmail.com>
2026-03-22 23:07:24 +0500

    Add delete btn.

--- Src/app.py
+++ Src/app.py
@@ -3,11 +3,7 @@ import customtkinter as ctk
 app = ctk.CTk()
 app.geometry("400x200")
 
-To_do_data = {
-    1: {"task": "Buy groceries", "status": "incomplete"},
-    2: {"task": "Finish project report", "status": "incomplete"}
-}
-
+To_do_data = {}
 
 def display_tasks():
     for task_id, task_info in To_do_data.items():
@@ -20,6 +16,30 @@ def display_tasks():
             corner_radius=10
         )
         task_label.grid(row=task_id + 1, column=0, pady=5)
+        button = ctk.CTkButton(
+            list_frame,
+            text="Mark Complete",
+            width=120,
+            height=40,
+            fg_color="darkgreen",
+            hover_color="#45a049",
+            corner_radius=10
+        )
+        button.grid(row=task_id + 1, column=1, padx=10)
+        button.configure(command=lambda id=task_id: mark_complete(id))
+        
+        delete_button = ctk.CTkButton(
+            list_frame,
+            text="Delete",
+            width=120,
+            height=40,
+            fg_color="darkred",
+            hover_color="#a52a2a",
+            corner_radius=10
+            )
+        delete_button.grid(row=task_id + 1, column=2, padx=10)
+        delete_button.configure(command=lambda id=task_id: delete_task(id)
+        )
 
 list_frame = ctk.CTkFrame(app)
 list_frame.pack(expand=True, padx=20, pady=20)
@@ -51,5 +71,14 @@ def get_data():
 
 button.configure(command=get_data)
 display_tasks()
+
+def mark_complete(task_id):
+    To_do_data[task_id]["status"] = "complete"
+    display_tasks()
+
+def delete_task(task_id):
+    del To_do_data[task_id]
+    # To_do_data.pop(task_id)
+    display_tasks()
 
 app.mainloop()
